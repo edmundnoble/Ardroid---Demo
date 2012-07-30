12c79059296c7e7869e22fd2411cc05c6281d49a
Edmund Noble <edmund@edmund-HAL-custom.(none)>
2012-07-29 21:04:49 -0400

    Works, now.

--- Client/ArduinoC/Release/subdir.mk
+++ Client/ArduinoC/Release/subdir.mk
@@ -17,7 +17,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: AVR C++ Compiler'
-	avr-g++ -I"/home/edmund/arduino-1.0.1/hardware/arduino/cores/arduino" -I"/home/edmund/arduino-1.0.1/hardware/arduino/variants/mega" -I"/home/edmund/workspac/ArduinoC" -I/home/edmund/arduino-1.0.1/libraries/Stepper -D__IN_ECLIPSE__=1 -DUSB_VID= -DUSB_PID= -DARDUINO=101 -Wall -Os -ffunction-sections -fdata-sections -fno-exceptions -g -mmcu=atmega2560 -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -c -o "$@" -x c++ "$<"
+	avr-g++ -I"/home/edmund/workspace" -I"/home/edmund/workspace" -I"/home/edmund/Ardroid---Demo/Client/ArduinoC" -I/home/edmund/arduino-1.0.1/libraries/Stepper -D__IN_ECLIPSE__=1 -DUSB_VID= -DUSB_PID= -DARDUINO= -Wall -Os -ffunction-sections -fdata-sections -fno-exceptions -g -mmcu=atmega2560 -DF_CPU=16000000UL -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)"  -c -o "$@" -x c++ "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 

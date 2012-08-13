118281583f9fbcfc21a03f0c1800b01ddcaa9f9d
Edmund Noble <edmund@edmund-HAL-custom.(none)>
2012-08-13 13:50:10 -0400

    Cleanup.

--- Client/ArduinoC/Release/arduino/WMath.d
+++
@@ -1,2 +0,0 @@
-arduino/WMath.d arduino/WMath.o: \
- /home/edmund/arduino-1.0.1/hardware/arduino/cores/arduino/WMath.cpp
--- Client/ArduinoC/Release/arduino/new.d
+++
@@ -1,5 +0,0 @@
-arduino/new.d arduino/new.o: \
- /home/edmund/arduino-1.0.1/hardware/arduino/cores/arduino/new.cpp \
- /home/edmund/arduino-1.0.1/hardware/arduino/cores/arduino/new.h
-
-/home/edmund/arduino-1.0.1/hardware/arduino/cores/arduino/new.h:
